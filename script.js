e51f763f144d357594c5ebc11a85c3eecc822f61
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-04-29 14:45:12 -0500

    testing js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let qNumber = document.querySelector('.questionNumber');
+let btn = document.querySelector('.answer');
+let num = 1;
+qNumber.innerHTML = 'Question' + num;
 
+btn.addEventListener('click', (e) => {
+    num++;
+    qNumber.innerHTML = 'Question' + num;
+});
