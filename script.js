77e29cec224bb6e84499e57fb876814531380568
TofuPanda1234567 <46355942+TofuPanda1234567@users.noreply.github.com>
2022-05-02 09:17:32 -0500

    added real question

--- script.js
+++ script.js
@@ -1,19 +1,50 @@
+// VARIABLES
 let qNumber = document.querySelector('.questionNumber');
 let ans1 = document.querySelector('.answer1');
 let ans2 = document.querySelector('.answer2');
 let ans3 = document.querySelector('.answer3');
+let scoreCount = document.querySelector('.score');
 let num = 1;
-qNumber.innerHTML = 'Question ' + num;
+let score = 0;
+let question = document.querySelector('.question');
 
+// LOAD HTML VALUES
+question.innerHTML = 'What is the most populated city in the world?';
+qNumber.innerHTML = 'Question ' + num;
+ans1.innerHTML = 'Tokyo';
+ans2.innerHTML = 'New York City';
+ans3.innerHTML = 'Manila';
+scoreCount.innerHTML = 'Score: ' + score;
+
+// ANSWER ONE
 ans1.addEventListener('click', (e) => {
     num++;
     qNumber.innerHTML = 'Question ' + num;
+
+    if(num == 1){
+        score = score++
+        score.innerHTML = 'Score: ' + score;
+    };
 });
+
+// ANSWER TWO
 ans2.addEventListener('click', (e) => {
     num++;
     qNumber.innerHTML = 'Question ' + num;
+
+    if(num == 1){
+        score = score;
+        score.innerHTML = 'Score: ' + score;
+    };
 });
+
+// ANSWER THREE
 ans3.addEventListener('click', (e) => {
     num++;
     qNumber.innerHTML = 'Question ' + num;
+
+    if(num == 1){
+        score = score
+        score.innerHTML = 'Score: ' + score;
+    };
 });
